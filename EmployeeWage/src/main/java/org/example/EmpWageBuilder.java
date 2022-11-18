890bd95d3a7a9a41cf53c3f7e3015e2a30323e2a
ratnadipbharde <ratnadipbharde@gmail.com>
2022-11-19 00:17:24 +0530

    Added Check Employee is Present or Absent solution

---
+++ EmployeeWage/src/main/java/org/example/EmpWageBuilder.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class EmpWageBuilder {
+    public static void main(String[] args) {
+       final int IS_FULL_TIME = 1;
+       double empCheck = Math.floor(Math.random()*10) % 2;
+       if (empCheck == IS_FULL_TIME)
+           System.out.println("Employee is Present");
+       else
+           System.out.println("Employee is Absent");
+    }
+}
