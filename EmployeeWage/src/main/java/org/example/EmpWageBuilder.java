4e49a1ed7201a8f06713cff8e9fa310e24ffc602
ratnadipbharde <ratnadipbharde@gmail.com>
2022-11-19 04:08:50 +0530

    Added Calculating Wages for a Month solution

--- EmployeeWage/src/main/java/org/example/EmpWageBuilder.java
+++ EmployeeWage/src/main/java/org/example/EmpWageBuilder.java
@@ -4,31 +4,33 @@ public class EmpWageBuilder {
     final int IS_FULL_TIME = 1;
     final int IS_PART_TIME = 2;
     final int EMP_RATE_PER_HOUR = 20;
+    final int NUM_OF_WORKING_DAYS = 20;
     int empHrs = 0;
     int empWage = 0;
 
     public static void main(String[] args) {
-        final int IS_FULL_TIME = 1;
-        double empCheck = Math.floor(Math.random() * 10) % 3;
+        int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
         EmpWageBuilder obj = new EmpWageBuilder();
         obj.isPresent(empCheck);
         System.out.println("Employee Wage=" + obj.getDailyWage(empCheck));
+        System.out.println("Employee Wage (Switch Statement)=" + obj.getDailyWageSwitchStatement(empCheck));
+        obj.getWagesForMonth();
     }
 
-    private void isPresent(double empCheck) {
-        if (empCheck == IS_FULL_TIME || empCheck == IS_PART_TIME)
-            System.out.println("Employee is Present");
-        else
-            System.out.println("Employee is Absent");
+    private void isPresent(int empCheck) {
+        if (empCheck == IS_FULL_TIME || empCheck == IS_PART_TIME) System.out.println("Employee is Present");
+        else System.out.println("Employee is Absent");
     }
 
-    int getDailyWage(double empCheck) {
-       /* if (empCheck == IS_FULL_TIME)
-            empHrs = 8;
-        else if (empCheck == IS_PART_TIME)
-            empHrs = 4; */
-        //using Switch Statement
-        switch ((int) empCheck) {
+    int getDailyWage(int empCheck) {
+        if (empCheck == IS_FULL_TIME) empHrs = 8;
+        else if (empCheck == IS_PART_TIME) empHrs = 4;
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        return empWage;
+    }
+
+    int getDailyWageSwitchStatement(int empCheck) {
+        switch (empCheck) {
             case IS_FULL_TIME:
                 empHrs = 8;
                 break;
@@ -36,9 +38,21 @@ public class EmpWageBuilder {
                 empHrs = 4;
                 break;
             default:
-                empHrs= 0;
+                empHrs = 0;
         }
         empWage = empHrs * EMP_RATE_PER_HOUR;
         return empWage;
+    }
+
+    private void getWagesForMonth() {
+        int totalWage = 0;
+        for (int i = 0; i < NUM_OF_WORKING_DAYS; i++) {
+            int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+            EmpWageBuilder obj1 = new EmpWageBuilder();
+            empWage = obj1.getDailyWageSwitchStatement(empCheck);
+            totalWage = totalWage + empWage;
+            System.out.println("emp Wage: " + empWage);
+        }
+        System.out.println("total Wages for a Month: " + totalWage);
     }
 }
