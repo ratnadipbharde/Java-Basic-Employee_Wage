94df9bb2a2eeb39aa89a2bf0de6228941db3bda4
ratnadipbharde <ratnadipbharde@gmail.com>
2022-11-19 01:08:33 +0530

    Added Calculate Daily Wage solution and create seprate UC by function

--- EmployeeWage/src/main/java/org/example/EmpWageBuilder.java
+++ EmployeeWage/src/main/java/org/example/EmpWageBuilder.java
@@ -1,12 +1,30 @@
 package org.example;
 
 public class EmpWageBuilder {
+    final int IS_FULL_TIME = 1;
+    final int EMP_RATE_PER_HOUR = 20;
+    int empHrs = 0;
+    int empWage = 0;
+
     public static void main(String[] args) {
-       final int IS_FULL_TIME = 1;
-       double empCheck = Math.floor(Math.random()*10) % 2;
-       if (empCheck == IS_FULL_TIME)
-           System.out.println("Employee is Present");
-       else
-           System.out.println("Employee is Absent");
+        final int IS_FULL_TIME = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        EmpWageBuilder obj = new EmpWageBuilder();
+        obj.isPresent(empCheck);
+        System.out.println("Employee Wage=" + obj.getDailyWage(empCheck));
+    }
+
+    private void isPresent(double empCheck) {
+        if (empCheck == IS_FULL_TIME)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
+    }
+
+    int getDailyWage(double empCheck) {
+        if (empCheck == IS_FULL_TIME)
+            empHrs = 8;
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        return empWage;
     }
 }
